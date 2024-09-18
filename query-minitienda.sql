36e2e13fb08ae276f0e36d1e01843a58410c3ca2
MANUEL ESTEBAN ERAZO MEDINA <1002820487@fup.edu>
2024-09-18 17:39:33 -0500

    feature-providers: se agrega el script con las queries de la sp de la tabla de proveedores

--- query-minitienda.sql
+++ query-minitienda.sql
@@ -65,7 +65,39 @@ END;
 DELIMITER ;
 
 -- Provedores
+-- INSERT
+DELIMITER //
+CREATE PROCEDURE sp_insert_provider(IN v_nit VARCHAR(45), IN v_nombre VARCHAR(100))
+BEGIN
+  INSERT INTO tbl_provedores(prov_nit, prov_nombre) VALUES(v_nit, v_nombre);
+END;
+DELIMITER ;
 
+-- UPDATE
+DELIMITER //
+CREATE PROCEDURE sp_update_provider(IN v_id INT, IN v_nit VARCHAR(45), IN v_nombre VARCHAR(100))
+BEGIN
+  UPDATE tbl_provedores
+  SET (prov_nit = v_nit, prov_nombre = v_nombre)
+  WHERE prov_id = v_id;
+END;
+DELIMITER ;
+
+-- SHOW
+DELIMITER //
+CREATE PROCEDURE sp_show_provider()
+BEGIN
+  SELECT prov_id, prov_nit, prov_nombre FROM tbl_provedores;
+END;
+DELIMITER ;
+
+-- DELETE
+DELIMITER //
+CREATE PROCEDURE sp_delete_provider(IN v_id INT)
+BEGIN
+  DELETE FROM tbl_provedores WHERE prov_id = v_id;
+END;
+DELIMITER ;
 
 -- Productos
 
